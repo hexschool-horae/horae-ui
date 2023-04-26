ca899719a67fda8e761dabce925659b668a41769
whatupmm <whatupmm11@gmail.com>
2023-04-26 15:39:43 +0800

    chore: add sass

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/tailwind.css";
 import type { AppProps } from "next/app";
+import "@/styles/app.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
