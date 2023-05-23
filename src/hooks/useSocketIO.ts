fdca9a76c355ac857c75c85bf6bfe5701f7f016a
James Jhong <jamesjhong@star-link.tech>
2023-05-23 11:51:53 +0800

    feat: 初始化 socket manager 的時候把 token 加入到 extra headers 裡面

--- src/hooks/useSocketIO.ts
+++ src/hooks/useSocketIO.ts
@@ -1,3 +1,9 @@
 import { Manager } from 'socket.io-client'
 const URL = process.env.NEXT_PUBLIC_SOCKET_SERVER || ''
-export default new Manager(URL)
+import store from '@/app/store'
+const token = store.getState()?.user?.token
+export default new Manager(URL, {
+  extraHeaders: {
+    token: `Bearer ${token}`,
+  },
+})
