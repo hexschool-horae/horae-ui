b42cf65753e3f72b5b30550ec129111a23ac6f79
James Jhong <jamesjhong@star-link.tech>
2023-05-27 02:04:17 +0800

    建立看板使用 boardService

--- src/pages/board/[boardId].tsx
+++ src/pages/board/[boardId].tsx
@@ -1,6 +1,6 @@
 import { MenuBar, List, AddListButton } from '@/components/board'
 import Head from 'next/head'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { DndContext } from '@dnd-kit/core'
 import { cloneDeep } from 'lodash-es'
 import { useBoardService } from '@/socketService'
@@ -9,33 +9,39 @@ import Draggable from '@/components/board/Draggable'
 import Droppable from '@/components/board/Droppable'
 import { useRouter } from 'next/router'
 
-const listCardList = [
-  {
-    title: '家居',
-    cardData: [
-      { title: '洗衣服', labels: ['未執行'] },
-      { title: '洗碗', labels: ['未執行'] },
-    ],
-  },
-  { title: '學習語言', cardData: [{ title: '英文聽力 L1', labels: ['已完成', '優先'] }] },
-  {
-    title: '待辦清單',
-    cardData: [
-      { title: '整理行事曆', labels: ['未執行', '優先'] },
-      { title: '匯款', labels: ['未執行', '優先'] },
-      { title: '買水果', labels: ['未執行'] },
-      { title: '整理舊衣服', labels: ['未執行', '中等'] },
-    ],
-  },
-  { title: '讀書進度', cardData: [{ title: '原子習慣 ch1', labels: ['進行中', '優先'] }] },
-  { title: '待整理資料', cardData: [{ title: 'React Hook', labels: ['未執行'] }] },
-]
+// const listCardList = [
+//   {
+//     title: '家居',
+//     cardData: [
+//       { title: '洗衣服', labels: ['未執行'] },
+//       { title: '洗碗', labels: ['未執行'] },
+//     ],
+//   },
+//   { title: '學習語言', cardData: [{ title: '英文聽力 L1', labels: ['已完成', '優先'] }] },
+//   {
+//     title: '待辦清單',
+//     cardData: [
+//       { title: '整理行事曆', labels: ['未執行', '優先'] },
+//       { title: '匯款', labels: ['未執行', '優先'] },
+//       { title: '買水果', labels: ['未執行'] },
+//       { title: '整理舊衣服', labels: ['未執行', '中等'] },
+//     ],
+//   },
+//   { title: '讀書進度', cardData: [{ title: '原子習慣 ch1', labels: ['進行中', '優先'] }] },
+//   { title: '待整理資料', cardData: [{ title: 'React Hook', labels: ['未執行'] }] },
+// ]
 
 export default function Board() {
-  const [list, setIsList] = useState(listCardList)
+  const [list, setIsList] = useState([])
+  const [boardId, setBoardId] = useState('')
   const router = useRouter()
-  const boardId = router.query.boardId as string
   const boardService = useBoardService()
+  useEffect(() => {
+    if (router.isReady) {
+      const boardId = router.query?.boardId as string
+      setBoardId(boardId)
+    }
+  }, [router.isReady])
   /* eslint-disable */
   function handleDragEnd(event: any) {
     console.log(event)
@@ -52,13 +58,12 @@ export default function Board() {
       setIsList(tempArr)
     }
   }
-
-  const onCreateList = (title: string = '') => {
+  const onCreateList = (title: string = '123') => {
     const payload = {
-      boardId,
       title,
+      boardId,
     }
-    boardService.createList(payload)
+    boardService?.createList(payload)
   }
   return (
     <>
