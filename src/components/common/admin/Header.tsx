29173b3bc8e6ceed829fcd3582ee505b9c625932
James Jhong <jamesjhong@star-link.tech>
2023-06-01 08:08:39 +0800

    加入 header

---
+++ src/components/common/admin/Header.tsx
@@ -0,0 +1,39 @@
+import { FC } from 'react'
+import IconLogo from '@/assets/icons/icon_logo.svg'
+import Link from 'next/link'
+import { InputText } from 'primereact/inputtext'
+
+interface IHeaderProps {
+  boardId?: string
+}
+
+const Header: FC<IHeaderProps> = ({ boardId }) => {
+  const headerStyle = (() => {
+    if (boardId) {
+      return 'bg-gray-3'
+    } else {
+      return 'bg-white'
+    }
+  })()
+  return (
+    <div className={`flex items-center py-4 px-[50px] text-black ${headerStyle}`}>
+      <IconLogo />
+      <Link href="/board" className="ml-8">
+        工作區
+      </Link>
+      <Link href="/board" className="ml-5">
+        最近的
+      </Link>
+      <Link href="/board" className="ml-5">
+        已標記
+      </Link>
+      <span className="p-input-icon-left ml-[200px]">
+        <i className="pi pi-search" />
+        <InputText placeholder="Search" />
+      </span>
+      <div className="w-[48px] h-[48px] rounded-full bg-black ml-auto"></div>
+    </div>
+  )
+}
+
+export default Header
