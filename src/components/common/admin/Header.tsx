ec7241a7f51aa5348e68d4c1df441a71170cff89
James Jhong <jamesjhong@star-link.tech>
2023-06-02 00:19:13 +0800

    增加登出功能

--- src/components/common/admin/Header.tsx
+++ src/components/common/admin/Header.tsx
@@ -2,12 +2,19 @@ import { FC } from 'react'
 import IconLogo from '@/assets/icons/icon_logo.svg'
 import Link from 'next/link'
 import { InputText } from 'primereact/inputtext'
+import { POST_USER_LOGOUT } from '@/apis/axios-service'
+import { useRouter } from 'next/router'
+import { AppDispatch } from '@/app/store'
+import { useDispatch } from 'react-redux'
+import { setIsLogin, setToken } from '@/slices/userSlice'
 
 interface IHeaderProps {
   boardId?: string
 }
 
 const Header: FC<IHeaderProps> = ({ boardId }) => {
+  const router = useRouter()
+  const dispatch = useDispatch<AppDispatch>()
   const headerStyle = (() => {
     if (boardId) {
       return 'bg-gray-3'
@@ -15,6 +22,18 @@ const Header: FC<IHeaderProps> = ({ boardId }) => {
       return 'bg-white'
     }
   })()
+
+  const onLogout = async () => {
+    try {
+      await POST_USER_LOGOUT()
+      dispatch(setIsLogin(false))
+      dispatch(setToken(''))
+      router.push('/')
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
   return (
     <div className={`flex items-center py-4 px-[50px] text-black border-b border-white ${headerStyle}`}>
       <IconLogo />
@@ -31,7 +50,12 @@ const Header: FC<IHeaderProps> = ({ boardId }) => {
         <i className="pi pi-search" />
         <InputText placeholder="Search" />
       </span>
-      <div className="w-[48px] h-[48px] rounded-full bg-black ml-auto"></div>
+      <div className="ml-auto flex items-center">
+        <span className="text-black mr-4 cursor-pointer" onClick={onLogout}>
+          登出
+        </span>
+        <div className="w-[48px] h-[48px] rounded-full bg-black ml-auto"></div>
+      </div>
     </div>
   )
 }
