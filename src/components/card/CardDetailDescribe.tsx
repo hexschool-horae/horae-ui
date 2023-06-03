3ceb8a14ca6fe9d2dc8b0eac926ce7b86b811f9c
Sasha Ye <akomat1103@gmail.com>
2023-06-03 23:46:48 +0800

    feat: dynamic載入編輯器

--- src/components/card/CardDetailDescribe.tsx
+++ src/components/card/CardDetailDescribe.tsx
@@ -1,40 +1,41 @@
-// import dynamic from "next/dynamic";
+import dynamic from 'next/dynamic'
 import remarkGfm from 'remark-gfm'
 import ReactMarkdown from 'react-markdown'
-// import MdEditor, { Plugins } from 'react-markdown-editor-lite';
-import MdEditor, { Plugins } from 'react-markdown-editor-lite'
+import { Plugins } from 'react-markdown-editor-lite'
+// import MdEditor, { Plugins } from 'react-markdown-editor-lite'
 import 'react-markdown-editor-lite/lib/index.css'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useCardDetail } from '@/contexts/cardDetailContext'
 import React from 'react'
 
-// const MdEditor = dynamic(
-//   /* @ts-ignore */
-//   () => {
-//     return new Promise((resolve) => {
-//       import("react-markdown-editor-lite").then((res) => {
-//         const Editor = res.default;
-//         Editor.addLocale("zh-TW", {
-//           btnHeader: "標頭",
-//           btnClear: "清除",
-//           btnBold: "粗體"
-//         });
-//         Editor.useLocale("zh-TW");
-//         Editor.unuse(Plugins.Image);
-//         resolve(Editor);
-//       });
-//     });
-// },
-// {
-//   ssr: false
-// });
+/* eslint-disable */
+const MdEditor = dynamic(
+  /* @ts-ignore */
+  () => {
+    return new Promise(resolve => {
+      import('react-markdown-editor-lite').then(res => {
+        const Editor = res.default
+        Editor.addLocale('zh-TW', {
+          btnHeader: '標頭',
+          btnClear: '清除',
+          btnBold: '粗體',
+        })
+        Editor.useLocale('zh-TW')
+        Editor.unuse(Plugins.Image)
+        resolve(Editor)
+      })
+    })
+  },
+  {
+    ssr: false,
+  }
+)
 
 export default function CardDetailDescribe() {
-  const editor = useRef(null)
   const { state, dispatch } = useCardDetail()
   const [description, setDescription] = useState('')
 
-  MdEditor.unuse(Plugins.Image)
+  // MdEditor.unuse(Plugins.Image)
   // eslint-disable-next-line
   const dispatchDescribe = () => {
     dispatch({
@@ -45,18 +46,41 @@ export default function CardDetailDescribe() {
     })
   }
 
+  const handleClick = () => {
+    // if (editor.current) {
+    //   // alert(editor.current.getMdValue());
+    // }
+  }
+
+  interface props {
+    html: string
+    text: string
+  }
+
+  const handleEditorChange = ({ html, text }: props) => {
+    const newValue = text.replace(/\d/g, '')
+    console.log(newValue)
+    console.log(html)
+    setDescription(newValue)
+  }
+
   useEffect(() => {
-    setDescription(state.cardDetail.describe)
+    // setDescription(state.cardDetail.describe)
+    setDescription('### ooo')
   }, [state.cardDetail.describe])
 
   return (
-    <MdEditor
-      ref={editor}
-      style={{ height: '240px' }}
-      placeholder="輸入描述..."
-      view={{ menu: true, html: true, md: true }}
-      // eslint-disable-next-line
-      renderHTML={(text: string) => <ReactMarkdown children={text} remarkPlugins={[remarkGfm]} />}
-    />
+    <>
+      <MdEditor
+        value={description}
+        style={{ height: '240px' }}
+        placeholder="輸入描述..."
+        view={{ menu: true, html: true, md: true }}
+        onChange={handleEditorChange}
+        // eslint-disable-next-line
+        renderHTML={(text: string) => <ReactMarkdown children={text} remarkPlugins={[remarkGfm]} />}
+      />
+      <button onClick={handleClick}>Get value</button>
+    </>
   )
 }
