80a2a38b98cd69a5ade51cdd60c830ec50a23cc4
Sasha Ye <akomat1103@gmail.com>
2023-06-04 01:37:27 +0800

    feat: 1.編輯器增加查看與編輯模式樣式2.顯示增加syntax highlighr

--- src/components/card/CardDetailDescribe.tsx
+++ src/components/card/CardDetailDescribe.tsx
@@ -1,12 +1,20 @@
 import dynamic from 'next/dynamic'
+import { Plugins } from 'react-markdown-editor-lite'
 import remarkGfm from 'remark-gfm'
 import ReactMarkdown from 'react-markdown'
-import { Plugins } from 'react-markdown-editor-lite'
-// import MdEditor, { Plugins } from 'react-markdown-editor-lite'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { github } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
 import 'react-markdown-editor-lite/lib/index.css'
+
 import { useEffect, useState } from 'react'
+import { Button } from 'primereact/button'
 import { useCardDetail } from '@/contexts/cardDetailContext'
-import React from 'react'
+import style from './cardDetail.module.scss'
+
+/*
+建議使用 client-side render 
+但會無法調用editor提供的方法，元件傳入的第一個參數會出現型別錯誤
+*/
 
 /* eslint-disable */
 const MdEditor = dynamic(
@@ -15,13 +23,9 @@ const MdEditor = dynamic(
     return new Promise(resolve => {
       import('react-markdown-editor-lite').then(res => {
         const Editor = res.default
-        Editor.addLocale('zh-TW', {
-          btnHeader: '標頭',
-          btnClear: '清除',
-          btnBold: '粗體',
-        })
-        Editor.useLocale('zh-TW')
+
         Editor.unuse(Plugins.Image)
+        Editor.unuse(Plugins.FontUnderline)
         resolve(Editor)
       })
     })
@@ -34,54 +38,84 @@ const MdEditor = dynamic(
 export default function CardDetailDescribe() {
   const { state, dispatch } = useCardDetail()
   const [description, setDescription] = useState('')
+  const [isEdit, setIsEdit] = useState(false)
 
-  // MdEditor.unuse(Plugins.Image)
-  // eslint-disable-next-line
+  const handleUpdate = () => {
+    dispatchDescribe()
+    setIsEdit(false)
+  }
+
   const dispatchDescribe = () => {
     dispatch({
-      type: 'SAVE_DESCRIPTION',
+      type: 'UPDATE_DESCRIBE',
       payload: {
         describe: description,
       },
     })
   }
 
-  const handleClick = () => {
-    // if (editor.current) {
-    //   // alert(editor.current.getMdValue());
-    // }
+  const handleCancel = () => {
+    setDescription(state.cardDetail.describe)
+    setIsEdit(false)
   }
 
-  interface props {
-    html: string
+  interface IEditorChangeProps {
+    // html: string
     text: string
   }
 
-  const handleEditorChange = ({ html, text }: props) => {
-    const newValue = text.replace(/\d/g, '')
-    console.log(newValue)
-    console.log(html)
-    setDescription(newValue)
+  const handleEditorChange = ({ text }: IEditorChangeProps) => {
+    setDescription(text)
   }
 
   useEffect(() => {
-    // setDescription(state.cardDetail.describe)
-    setDescription('### ooo')
+    setDescription(state.cardDetail.describe)
   }, [state.cardDetail.describe])
 
   return (
-    <>
-      <MdEditor
-        /* @ts-ignore */
-        value={description}
-        style={{ height: '240px' }}
-        placeholder="輸入描述..."
-        view={{ menu: true, html: true, md: true }}
-        onChange={handleEditorChange}
-        // eslint-disable-next-line
-        renderHTML={(text: string) => <ReactMarkdown children={text} remarkPlugins={[remarkGfm]} />}
-      />
-      <button onClick={handleClick}>Get value</button>
-    </>
+    <div className="my-5" onClick={() => !isEdit && setIsEdit(true)}>
+      <div className="relative mb-2">
+        <MdEditor
+          /* @ts-ignore */
+          htmlClass={`${style.editor_preview} custom-html-style`}
+          value={description}
+          style={{ height: '240px' }}
+          placeholder="輸入描述..."
+          onChange={handleEditorChange}
+          /* eslint-disable */
+          renderHTML={(text: string) => (
+            <ReactMarkdown
+              children={text}
+              remarkPlugins={[remarkGfm]}
+              components={{
+                code({ node, inline, className, children, ...props }) {
+                  const match = /language-(\w+)/.exec(className || '')
+                  return !inline && match ? (
+                    <SyntaxHighlighter
+                      {...props}
+                      children={String(children).replace(/\n$/, '')}
+                      style={github}
+                      language={match[1]}
+                      PreTag="div"
+                    />
+                  ) : (
+                    <code {...props} className={className}>
+                      {children}
+                    </code>
+                  )
+                },
+              }}
+            />
+          )}
+        />
+        {!isEdit && <div className={`${style.editor_overlay}`}></div>}
+      </div>
+      {isEdit && (
+        <div className="flex justify-end gap-2">
+          <Button label="取消" severity="secondary" rounded outlined size="small" onClick={handleCancel} />
+          <Button label="儲存" severity="secondary" rounded size="small" onClick={handleUpdate} />
+        </div>
+      )}
+    </div>
   )
 }
