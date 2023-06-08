4377d1fcff5508fffcc3d165dd8a18ded2cb5a92
James Jhong <jamesjhong@star-link.tech>
2023-06-09 01:52:53 +0800

    調整 default layout 寬高

--- src/components/layout/DefaultLayout.tsx
+++ src/components/layout/DefaultLayout.tsx
@@ -7,10 +7,10 @@ export default function DefaultLayout({ children }: { children?: ReactNode }) {
   const router = useRouter()
   const showFrontHeader = router.pathname === '/'
   return (
-    <>
+    <div className="flex flex-col h-full">
       {showFrontHeader && <FrontHeader />}
-      <main className="main">{children}</main>
+      <main className="flex-1">{children}</main>
       <FrontFooter />
-    </>
+    </div>
   )
 }
