331daa930527df09db2911605afe4884fa2c74aa
James Jhong <jamesjhong@star-link.tech>
2023-06-10 17:49:39 +0800

    modify userSlice

--- src/slices/userSlice.ts
+++ src/slices/userSlice.ts
@@ -1,7 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
-import { persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
 
 interface IUserInitialState {
   isLogin: boolean
@@ -20,12 +18,6 @@ const initialState: IUserInitialState = {
   },
 }
 
-const userPersistConfig = {
-  key: 'user',
-  storage: storage,
-  whitelist: ['token'], // 只保存 'token' 字段到本地存储
-}
-
 export const userSlice = createSlice({
   name: 'user',
   initialState,
@@ -37,7 +29,6 @@ export const userSlice = createSlice({
       state.token = action.payload
     },
     setProfile(state, action: PayloadAction<string>) {
-      console.log('setProfile = ', action.payload, state.profile)
       state.profile.email = action.payload
     },
   },
@@ -45,4 +36,4 @@ export const userSlice = createSlice({
 
 export const { setIsLogin, setToken, setProfile } = userSlice.actions //給React組件個別使用
 
-export default persistReducer(userPersistConfig, userSlice.reducer) //給store.js使用
+export default userSlice.reducer
