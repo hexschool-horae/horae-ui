30612d14ff59f2e16e78aaf3ebef6afa6988aead
James Jhong <jamesjhong@star-link.tech>
2023-06-10 17:32:11 +0800

    sidebar 每個項目加入收合特效

--- src/components/common/admin/Sidebar.tsx
+++ src/components/common/admin/Sidebar.tsx
@@ -4,14 +4,20 @@ import WorkSpaceModel from '@/components/workSpace/WorkSpaceModel'
 import { IUserBoardDataRes } from '@/apis/interface/api'
 import { GET_ALL_WORK_SPACE } from '@/apis/axios-service'
 import Link from 'next/link'
-import IconLeftArrow from '@/assets/icons/icon_left_arrow.svg'
 import IconBoard from '@/assets/icons/icon_board.svg'
 import IconWorkspace from '@/assets/icons/icon_workspace.svg'
 import IconAdd from '@/assets/icons/icon_add.svg'
+import IconArrowDown from '@/assets/icons/icon_arrow_down.svg'
+
 interface ISidebarProps {
   className?: string
   boardId?: string
   alwaysHide?: boolean | null
+}
+
+interface IUserBoardMenuToggleStatus {
+  id: string
+  active: boolean
 }
 
 const Sidebar: FC<ISidebarProps> = ({ className, boardId }) => {
@@ -23,6 +29,7 @@ const Sidebar: FC<ISidebarProps> = ({ className, boardId }) => {
     }
   })()
   const [userBoardList, setUserBoardList] = useState<IUserBoardDataRes[]>([])
+  const [userBoardMenuToggleStatus, setUserBoardMenuToggleStatus] = useState<IUserBoardMenuToggleStatus[]>([])
   const [showWorkSpaceModal, setShowWorkSpaceModal] = useState(false)
   const showDialog = () => {
     setShowWorkSpaceModal(true)
@@ -37,9 +44,28 @@ const Sidebar: FC<ISidebarProps> = ({ className, boardId }) => {
     try {
       const result = await GET_ALL_WORK_SPACE()
       setUserBoardList(result.data)
+      const initialMenuToggleStatus = result.data.map(item => ({
+        id: item._id,
+        active: true,
+      }))
+      setUserBoardMenuToggleStatus(initialMenuToggleStatus)
     } catch (e) {
       console.warn(e)
     }
+  }
+
+  const onToggleMenu = (titleId: string) => {
+    setUserBoardMenuToggleStatus(prevState =>
+      prevState.map(item => {
+        if (item.id === titleId) {
+          return {
+            ...item,
+            active: !item.active,
+          }
+        }
+        return item
+      })
+    )
   }
 
   useEffect(() => {
@@ -50,9 +76,7 @@ const Sidebar: FC<ISidebarProps> = ({ className, boardId }) => {
   }
 
   return (
-    <div
-      className={`w-[332px] relative p-5 inline-flex flex-col text-base leading-none text-black ${className} ${sidebarStyle}`}
-    >
+    <div className={`hide-scrollbar ${classes.sidebar} ${className} ${sidebarStyle}`}>
       <Link href="/board" className="cursor-pointer">
         <div className="flex items-center">
           <IconBoard />
@@ -68,11 +92,20 @@ const Sidebar: FC<ISidebarProps> = ({ className, boardId }) => {
         {userBoardList.map((item, index) => {
           return (
             <div className={classes['workspace-list-item']} key={index}>
-              <div className="flex items-center">
+              <div className="flex items-center cursor-pointer select-none" onClick={() => onToggleMenu(item._id)}>
                 <span className="bg-primary text-white rounded py-1.5 px-[10px] mr-3">{getShortName(item.title)}</span>
                 <span>{item.title}</span>
+                <IconArrowDown
+                  className={`w-4 h-4 ml-auto ${classes['icon-arrow']} ${
+                    userBoardMenuToggleStatus[index].active ? `${classes.active}` : ''
+                  }`}
+                />
               </div>
-              <div className="flex flex-col ml-[45px] mt-4">
+              <div
+                className={`flex flex-col ml-[45px] mt-4 ${classes.menus} ${
+                  userBoardMenuToggleStatus[index].active ? `${classes.active}` : ''
+                }`}
+              >
                 <Link href={`/workspace/${item._id}/home`}>看板</Link>
                 <Link href={`/workspace/${item._id}/members`} className="mt-3">
                   成員
@@ -85,11 +118,7 @@ const Sidebar: FC<ISidebarProps> = ({ className, boardId }) => {
           )
         })}
       </div>
-      <div className="absolute right-0 top-[30px] transform translate-x-1/2">
-        <div className={classes['toggle-btn']}>
-          <IconLeftArrow />
-        </div>
-      </div>
+
       <WorkSpaceModel
         visible={showWorkSpaceModal}
         onHide={hideDialog}
