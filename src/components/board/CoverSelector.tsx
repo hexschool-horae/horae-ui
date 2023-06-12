075bd6080a802a8d2acbc91303583ccc7cf16048
Mia <sumiret.6639@gmail.com>
2023-06-12 23:34:11 +0800

    feat: 新增選擇看板封面圖元件、新增修改列表標題功能、調整看板邀請頁

--- src/components/board/CoverSelector.tsx
+++ src/components/board/CoverSelector.tsx
@@ -1,16 +1,18 @@
 import { memo, useEffect, useState } from 'react'
 import axios from 'axios'
-// import { Button } from 'primereact/button'
+import Style from './CoverSelector.module.scss'
+import { Button } from 'primereact/button'
+import { classNames } from 'primereact/utils'
 
-// type TCoverType = 'local' | 'unsplash' | 'color' | null
+type TCoverType = 'local' | 'unsplash' | 'theme' | null
 
 const CoverSelector = () => {
   const [imgList, setImgList] = useState<string[]>([])
-  // const [coverType, setCoverType] = useState<TCoverType>(null)
+  const [coverType, setCoverType] = useState<TCoverType>(null)
 
   const hanldeGetUnsplash = async () => {
     const result = await axios.get(
-      'https://api.unsplash.com/photos/?client_id=g37xgq9_G4tU9dk1fT3TWVJnPR-4jzDBbGSTji-WLME'
+      'https://api.unsplash.com/photos/random?client_id=g37xgq9_G4tU9dk1fT3TWVJnPR-4jzDBbGSTji-WLME&count=8&orientation=landscape'
     )
     if (result) {
       const { data } = result
@@ -18,42 +20,92 @@ const CoverSelector = () => {
       const list = data.map((item: any) => item?.urls?.thumb)
       setImgList(list)
     }
-    console.log(result)
   }
 
-  // useEffect(() => {
-  //   if (coverType === 'unsplash') {
-  //   hanldeGetUnsplash()
-  //   }
-  // }, [coverType])
+  const handleSetCoverType = (type: TCoverType) => {
+    setTimeout(() => {
+      setCoverType(type)
+    }, 1000)
+  }
+
+  const handleUpdateCover = (type: TCoverType) => {
+    console.log(type)
+  }
 
   useEffect(() => {
-    // if (coverType === 'unsplash') {
-    hanldeGetUnsplash()
-    // }
-  }, [])
+    if (coverType === 'unsplash') {
+      hanldeGetUnsplash()
+    }
+  }, [coverType])
 
   return (
-    <div className="p-8">
-      <h5 className="text-center">更換背景</h5>
-      <div className="flex flex-col items-center">
-        {/* {coverType === null && (
-          <div className="mr-4">
-            <Button onClick={() => setCoverType('unsplash')}>照片</Button>
-            <Button severity="secondary" onClick={() => setCoverType('color')}>
-              顏色
-            </Button>
-          </div>
-        )} */}
+    <div className="p-8 w-[400px]">
+      <div className="w-full flex items-center mb-4">
+        {coverType !== null && (
+          <Button
+            className="p-0"
+            icon="pi pi-angle-left"
+            severity="secondary"
+            text
+            onClick={() => handleSetCoverType(null)}
+          />
+        )}
 
-        {/* {coverType === 'unsplash' && ( */}
-        <div className="w-[200px] flex flex-wrap">
+        <h5 className="w-full text-center">更換背景</h5>
+
+        {coverType === 'unsplash' && (
+          <Button className="p-0" icon="pi pi-sync" severity="secondary" text onClick={hanldeGetUnsplash} />
+        )}
+      </div>
+
+      {coverType === null && (
+        <div className="w-full flex mb-5">
+          <div className="flex-1 mr-2">
+            <div
+              className={classNames('bg-secondary-3', Style.cover_selector_item)}
+              onClick={() => handleSetCoverType('unsplash')}
+            ></div>
+            <div className="text-center">照片</div>
+          </div>
+
+          <div className="flex-1 ">
+            <div
+              className={classNames('bg-secondary-4', Style.cover_selector_item)}
+              onClick={() => handleSetCoverType('theme')}
+            ></div>
+            <div className="text-center">顏色</div>
+          </div>
+        </div>
+      )}
+
+      {/* 從 unsplash 選擇 */}
+      {coverType === 'unsplash' && (
+        <div className="w-full flex flex-wrap justify-between">
           {imgList.map((item, index: number) => (
-            <img width={50} height={50} key={index} src={item} />
+            <div key={index} className={Style.img_item} onClick={() => handleUpdateCover('unsplash')}>
+              <img style={{ width: '100%', height: '100px' }} src={item} />
+            </div>
           ))}
         </div>
-        {/* )} */}
-      </div>
+      )}
+
+      {/* 從主題選擇 */}
+      {coverType === 'theme' && (
+        <div className="w-full flex mb-5">
+          <div className="flex-1 mr-2">
+            <div
+              className={classNames('bg-gray-2', Style.theme_item)}
+              onClick={() => handleSetCoverType('theme')}
+            ></div>
+          </div>
+          <div className="flex-1  mr-2">
+            <div
+              className={classNames('bg-gray-3', Style.theme_item)}
+              onClick={() => handleSetCoverType('theme')}
+            ></div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
--- src/components/board/List.tsx
+++ src/components/board/List.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { memo, useState } from 'react'
-
+import { memo, useState, useRef, useEffect } from 'react'
+// import { useAppSelector } from '@/hooks/useAppStore'
+// import { useAppDispatch } from '@/hooks/useAppStore'
+import { InputText } from 'primereact/inputtext'
 import AddCardButton from './AddCardButton'
 import ListSettingMenu from './ListSettingMenu'
 import Card from './Card'
@@ -16,13 +18,35 @@ const List = ({ data }: { data: IBoardListItem; onCreateCard?: (listId: string, 
   /** 卡片陣列狀態 */
   const { cards } = data
   const router = useRouter()
-
+  // const dispatch = useAppDispatch()
   const [isDisabledLink, setIsDisabledLink] = useState(false)
+  const [isEdit, setIsEdit] = useState(false)
+  const listTitle = data?.title
+  const [localListTitle, setLocalListTitle] = useState(data.title || '')
+  const inputRef = useRef<HTMLInputElement>(null)
 
   const onIsDragging = (isDragging: boolean) => {
     /** 當拖曳事件發生時，禁止 Link 被觸發(透過 class disabled-link) */
     setIsDisabledLink(isDragging)
   }
+
+  useEffect(() => {
+    if (isEdit) {
+      if (inputRef) {
+        inputRef.current?.focus()
+      }
+    } else {
+      if (localListTitle !== listTitle) {
+        // dispatch(
+        //   socketServiceActions.modifyBoardTitle({
+        //     boardId,
+        //     title: listTitle,
+        //   })
+        // )
+        // dispatch(dialogSliceActions.pushSpinnerQueue(SOCKET_EVENTS_ENUM.BOARD_MODIFY_TITLE_RESULT))
+      }
+    }
+  }, [isEdit])
 
   return (
     <>
@@ -33,8 +57,23 @@ const List = ({ data }: { data: IBoardListItem; onCreateCard?: (listId: string, 
           eventType: 'list-top',
         }}
       >
-        <div className="flex mb-3">
-          <h6 className="text-lg !text-secondary-3 mr-auto ">{data.title}</h6>
+        <div
+          className="flex mb-3"
+          onClick={() => {
+            setIsEdit(true)
+          }}
+          onBlur={() => setIsEdit(false)}
+        >
+          {isEdit ? (
+            <InputText
+              className="w-full"
+              ref={inputRef}
+              value={localListTitle}
+              onChange={e => setLocalListTitle(e.target.value)}
+            />
+          ) : (
+            <h6 className="text-lg !text-secondary-3 mr-auto ">{listTitle}</h6>
+          )}
           <ListSettingMenu />
         </div>
       </Droppable>
