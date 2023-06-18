8b3024ba2bf280e76f6c75e55665b1ce1319ea61
Mia <sumiret.6639@gmail.com>
2023-06-18 23:17:51 +0800

    fix: add list component

--- src/components/board/index.ts
+++ src/components/board/index.ts
@@ -1,3 +1,3 @@
 export { default as MenuBar } from './MenuBar'
-export { default as List } from './List'
+// export { default as List } from './List'
 export { default as AddListButton } from './AddListButton'
